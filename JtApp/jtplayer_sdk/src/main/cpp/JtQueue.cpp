579bf516a16d8fe941000a6e3425a971e9af07a2
cjtstrive <1346618876@qq.com>
2020-10-10 16:07:05 +0800

    modify queue

--- JtApp/jtplayer_sdk/src/main/cpp/JtQueue.cpp
+++ JtApp/jtplayer_sdk/src/main/cpp/JtQueue.cpp
@@ -3,3 +3,75 @@
 //
 
 #include "JtQueue.h"
+
+
+JtQueue::JtQueue(JtPlaystatus *playstatus) {
+	this->_pPlayState = playstatus;
+	pthread_mutex_init(&_mutex, NULL);
+	pthread_cond_init(&_cond, NULL);
+}
+
+
+JtQueue::~JtQueue() {
+	clearQueue();
+    pthread_mutex_destroy(&_mutex);
+    pthread_cond_destroy(&_cond);
+};
+
+
+int JtQueue::pushAvPacket(AVPacket *&avpacket) {
+	pthread_mutex_lock(&_mutex);
+	if(_queueAvpacket.size() >= 30) {
+		pthread_cond_signal(&_cond);
+		pthread_mutex_unlock(&_mutex);
+		return -1;
+	}else {
+		_queueAvpacket.push(avpacket);
+		pthread_cond_signal(&_cond);
+		pthread_mutex_unlock(&_mutex);
+		return 0;
+	}
+}
+
+
+int JtQueue::popAvPacket(AVPacket** avpacket) {
+	pthread_mutex_lock(&_mutex);
+	if(_queueAvpacket.empty()) {
+		pthread_cond_wait(&_cond, &_mutex);
+	}
+    AVPacket *tmpPacket =  _queueAvpacket.front();
+    if(av_packet_ref(*avpacket, tmpPacket) == 0)
+    {
+    	_queueAvpacket.pop();
+    }
+    av_packet_free(&tmpPacket);
+    av_free(tmpPacket);
+    tmpPacket = NULL;
+	pthread_mutex_unlock(&_mutex);
+	return 0;
+}
+
+
+
+int JtQueue::getQueueSize() {
+    int queueSize = 0;
+    pthread_mutex_lock(&_mutex);
+    queueSize = _queueAvpacket.size();
+    pthread_mutex_unlock(&_mutex);
+    return queueSize;
+};
+
+
+
+int JtQueue::clearQueue() {
+	pthread_mutex_lock(&_mutex);
+	if(!_queueAvpacket.empty()) {
+        AVPacket *packet = _queueAvpacket.front();
+        _queueAvpacket.pop();
+        av_packet_free(&packet);
+        av_free(packet);
+        packet = NULL;
+	}
+	pthread_mutex_unlock(&_mutex);
+	return 0;
+}
--- JtApp/jtplayer_sdk/src/main/cpp/JtQueue.h
+++ JtApp/jtplayer_sdk/src/main/cpp/JtQueue.h
@@ -5,13 +5,33 @@
 #ifndef JTAPP_JTQUEUE_H
 #define JTAPP_JTQUEUE_H
 
+extern "C"{
+#include <libavutil/imgutils.h>
+#include <libavutil/samplefmt.h>
+#include <libavutil/timestamp.h>
+#include <libavformat/avformat.h>
+}
+#include "pthread.h"
+#include "JtPlaystatus.h"
+#include <vector>
+#include <queue>
+
 
 class JtQueue {
 
 public:
-    JtQueue(JtPlaystatus *playstatus);
+    JtQueue(JtPlaystatus* playstatus);
+    ~JtQueue();
+	int pushAvPacket(AVPacket *&avpacket);
+	int popAvPacket(AVPacket** avpacket);
+	int getQueueSize();
+	int clearQueue();
 private:
-    JtPlaystatus *_pPlayState;
+    JtPlaystatus *_pPlayState = NULL;
+	pthread_mutex_t _mutex;
+	pthread_cond_t _cond;
+	std::queue<AVPacket *> _queueAvpacket;
+	
 };
 
 
